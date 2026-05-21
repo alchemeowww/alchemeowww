106626b0e96122a6e7dc374e1a2aeb37b5bdf15c
Tan Lee Kent <kent.1991@live.com>
2026-05-21 14:39:09 +0800

    update GEO

--- nextjs/src/app/robots.ts
+++ nextjs/src/app/robots.ts
@@ -4,7 +4,18 @@ export const dynamic = "force-static";
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: { userAgent: "*", allow: "/" },
+    rules: [
+      {
+        userAgent: "*",
+        allow: "/",
+        disallow: ["/nextjs/", "/test/"],
+      },
+      { userAgent: "GPTBot", allow: "/" },
+      { userAgent: "ChatGPT-User", allow: "/" },
+      { userAgent: "ClaudeBot", allow: "/" },
+      { userAgent: "PerplexityBot", allow: "/" },
+      { userAgent: "Google-Extended", allow: "/" },
+    ],
     sitemap: "https://alchemeowww.com/sitemap.xml",
   };
 }
